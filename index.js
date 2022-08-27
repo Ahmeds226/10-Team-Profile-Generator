34783c0c8b7afa272031ee2de0236cfa292d37d7
Ahmed Shahnawaz <iahmed.s1@icloud.com>
2022-08-27 11:28:50 +0100

    Added functions to Index.js

--- index.js
+++ index.js
@@ -10,6 +10,47 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 teamArray = [];
 
 function runApp() {
+  // Add Manager Function
+  function addManager() {
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "managerName",
+          message: "Please enter your Manager's name",
+        },
+
+        {
+          type: "input",
+          name: "managerId",
+          message: "Please enter your Manager's ID number",
+        },
+
+        {
+          type: "input",
+          name: "managerEmail",
+          message: "Please enter you Manager's email address",
+        },
+
+        {
+          type: "input",
+          name: "managerPhone",
+          message: "Please enter yout Manager's phone number",
+        },
+      ])
+      .then((answers) => {
+        const manager = new Manager(
+          answers.managerName,
+          answers.managerId,
+          answers.managerEmail,
+          answers.managerPhone
+        );
+        teamArray.push(manager);
+        createTeam();
+      });
+  }
+
+  // Add Engineer Function
   function addEngineer() {
     inquirer
       .prompt([
@@ -46,6 +87,48 @@ function runApp() {
       });
   }
 
+  // Add Intern Function
+  function addIntern() {
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "internName",
+          message: "Please enter the Intern's full name",
+        },
+
+        {
+          type: "input",
+          name: "internId",
+          message: "Please enter the Intern's ID number",
+        },
+
+        {
+          type: "input",
+          name: "internEmail",
+          message: "Please enter the Intern's email address",
+        },
+
+        {
+          type: "input",
+          name: "internSchool",
+          message:
+            "Please enter the school the Intern's is currently attending",
+        },
+      ])
+      .then((answers) => {
+        const intern = new Intern(
+          answers.internName,
+          answers.internId,
+          answers.internEmail,
+          answers.internSchool
+        );
+        teamArray.push(intern);
+        createTeam();
+      });
+  }
+
+  // Create team function
   function createTeam() {
     console.log("Team successfully created!");
   }
